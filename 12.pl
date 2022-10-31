e10beb357e0f82a4c273d1753cc69a71f7876a74
Vinícius Botelho <68855615+viniciusdev-br@users.noreply.github.com>
2022-10-31 16:11:07 -0300

    Update 12.pl

--- 12.pl
+++ 12.pl
@@ -30,3 +30,51 @@ remDupl1([X|Xs], [X,Xd]):- remoDupl1(Xs, Xd).
 % 20
 remove(X,[X|Xs], Xr):- remove(X,Xs,Xr).
 remove(X, [Y|Xs], [Y,Xr]):- X =/=, remove(X,Xs,Xr).
+
+
+% 39
+
+particiona([X,Y|XYs],[X|Xs], [Y,Ys]) :- particiona(XYs, Xs, Ys).
+particiona([X], [X], []).
+particiona([],[],[]).
+
+merge1([X|Xs],[Y|Ys],[X|XYs]):-X@<Y,!,merge1(Xs, [Y|Ys],XYs).
+merge1([X|Xs],[Y|Ys],[Y|XYs]):-X@>=Y,!,merge1([X|Xs], Ys,XYs).
+merge1([],Ys,Ys):-!.
+merge1(Xs,[],Xs):-!. 
+
+%41
+mergeSort([],[]):-!.
+mergeSort([X],[X]):-!.
+mergeSort(L,S):-particiona(L,X,Y),mergeSort(X,Xo),mergeSort(Y,Yo),merge1(Xo,Yo,S). 
+
+%42
+enezimo(N,[X|Xs],X):-N<=1.
+enezimo(N,[X|Xs],Y):-N>1, N1 is N-1, enezimo(N1,Xs,Y). 
+
+%43
+compr2(L,C):-L=[],C=0. 
+compr2(L,C):-L=[X|Xs],compr2(Xs,C1),C is C1+1. 
+
+maxL0([X],X).
+maxL0([X|Xs],X):-maxL0(Xs,M),X >M,!.
+maxL0([X|Xs],M):-maxL0(Xs,M),X=<M,!.
+%%
+maxL00([X],X).
+maxL00([X|Xs],M):-maxL00(Xs,M1),(X>M1,M=X,!; X=<M1,M=M1).
+%%
+maxL([X|Xs],M):-maxL(Xs,X,M).
+maxL([],M,M).
+maxL([X|Xs],ACC,M):- ACC>X,!,maxL(Xs,ACC,M).
+maxL([X|Xs],_,M):- maxL(Xs,X,M).
+%%
+gL(L,N):-!,findall(X,(between(1,N,I),X is random(1000)),L). 
+
+
+
+
+rsort(X,S):-sort(X,S1),reverse(S1,S).
+rmsort(X,S):-msort(X,S1),reverse(S1,S). 
+
+front(N,L,Lo):-length(Lo,N),append(Lo,_,L).
+last(N,L,Lo):-length(Lo,N),append(_,Lo,L). 
